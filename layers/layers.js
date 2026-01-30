864e3c40354f6c7337633d24b872207d8867fba4
Poligonais <cgpg@mpor.gov.br>
2026-01-30 10:37:43 -0300

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,20 +1,21 @@
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:4326").setExtent([-54.823261, -2.423578, -54.698200, -2.356889]);
 var wms_layers = [];
 
 
         var lyr_GoogleSatellite_0 = new ol.layer.Tile({
             'title': 'Google Satellite',
-            //'type': 'base',
             'opacity': 1.000000,
             
             
             source: new ol.source.XYZ({
-    attributions: ' ',
+            attributions: ' ',
                 url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
             })
         });
 var format_readoPortoOrganizadodeSantarm_1 = new ol.format.GeoJSON();
 var features_readoPortoOrganizadodeSantarm_1 = format_readoPortoOrganizadodeSantarm_1.readFeatures(json_readoPortoOrganizadodeSantarm_1, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
 var jsonSource_readoPortoOrganizadodeSantarm_1 = new ol.source.Vector({
     attributions: ' ',
 });
@@ -23,16 +24,16 @@ var lyr_readoPortoOrganizadodeSantarm_1 = new ol.layer.Vector({
                 declutter: false,
                 source:jsonSource_readoPortoOrganizadodeSantarm_1, 
                 style: style_readoPortoOrganizadodeSantarm_1,
-                popuplayertitle: "Área do Porto Organizado de Santarém",
+                popuplayertitle: 'Área do Porto Organizado de Santarém',
                 interactive: true,
                 title: '<img src="styles/legend/readoPortoOrganizadodeSantarm_1.png" /> Área do Porto Organizado de Santarém'
             });
 
 lyr_GoogleSatellite_0.setVisible(true);lyr_readoPortoOrganizadodeSantarm_1.setVisible(true);
 var layersList = [lyr_GoogleSatellite_0,lyr_readoPortoOrganizadodeSantarm_1];
-lyr_readoPortoOrganizadodeSantarm_1.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
-lyr_readoPortoOrganizadodeSantarm_1.set('fieldImages', {'Name': 'TextEdit', 'description': 'TextEdit', 'timestamp': 'DateTime', 'begin': 'DateTime', 'end': 'DateTime', 'altitudeMode': 'TextEdit', 'tessellate': 'Range', 'extrude': 'Range', 'visibility': 'Range', 'drawOrder': 'Range', 'icon': 'TextEdit', });
-lyr_readoPortoOrganizadodeSantarm_1.set('fieldLabels', {'Name': 'inline label - always visible', 'description': 'hidden field', 'timestamp': 'hidden field', 'begin': 'hidden field', 'end': 'hidden field', 'altitudeMode': 'hidden field', 'tessellate': 'hidden field', 'extrude': 'hidden field', 'visibility': 'hidden field', 'drawOrder': 'hidden field', 'icon': 'hidden field', });
+lyr_readoPortoOrganizadodeSantarm_1.set('fieldAliases', {'id': 'id', 'Name': 'Nome', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_readoPortoOrganizadodeSantarm_1.set('fieldImages', {'id': 'TextEdit', 'Name': 'TextEdit', 'description': 'TextEdit', 'timestamp': 'DateTime', 'begin': 'DateTime', 'end': 'DateTime', 'altitudeMode': 'TextEdit', 'tessellate': 'Range', 'extrude': 'Range', 'visibility': 'Range', 'drawOrder': 'Range', 'icon': 'TextEdit', });
+lyr_readoPortoOrganizadodeSantarm_1.set('fieldLabels', {'id': 'hidden field', 'Name': 'inline label - visible with data', 'description': 'hidden field', 'timestamp': 'hidden field', 'begin': 'hidden field', 'end': 'hidden field', 'altitudeMode': 'hidden field', 'tessellate': 'hidden field', 'extrude': 'hidden field', 'visibility': 'hidden field', 'drawOrder': 'hidden field', 'icon': 'hidden field', });
 lyr_readoPortoOrganizadodeSantarm_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
